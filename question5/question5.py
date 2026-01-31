19d0579efa60998c1c7c0e75ea3711983d8b0729
Nischal Shrestha <sthanischal5060@gmail.com>
2026-01-31 20:41:05 +0545

    question 5 small change

--- question5/question5.py
+++ question5/question5.py
@@ -5,11 +5,10 @@ with open("employees.txt","r") as f1:
     with open("management.txt","w") as f2:
         while True:
             text=f1.readline()
-            if 'Python' in text:
-                f2.write(text)
-            else :
-                pass
             if text=="":
                 break
             
+            if 'Python' in text:
+                f2.write(text)
+
         
